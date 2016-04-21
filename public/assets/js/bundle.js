f5a1983d36a4c69d2b375eb20aaeea735317c424
Paulo Silva <engelik@gmail.com>
2016-04-21 14:07:32 +0100

    added missing pages

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -1,6 +1,22 @@
 
-var main = function() {
-	
+'use strict';
+var main = function () {
+    
+    var overlay = $('#overlay');
+	$('.typeimage>.view').click(function (event) {
+      event.preventDefault();
+      var imgPath = $(this).attr('href');
+//      alert('hii' + $(this).attr('href'));
+      var imgtag = '<img src="' + imgPath +'" >';
+      overlay.html(imgtag);
+      overlay.fadeIn(300);
+    });
+    
+    overlay.click(function () {
+        overlay.hide();
+        overlay.html('');
+      }
+    );
 };
 // end of main function
 
